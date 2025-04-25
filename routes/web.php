560bade4a91d1e2ccd024f466a2d3d3b622efaf3
anivz25 <aduanavina@gmail.com>
2025-04-25 18:55:26 +0800

    to views authentication

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,23 @@ Route::get('/', function () {
 
 Route::resource('products', ProductController::class);
 
+
+Route::get('/', fn () => view('welcome'));
+
+// Guest routes
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+
+    Route::get('/register', [RegisterController::class, 'showRegister'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+// Authenticated route
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
